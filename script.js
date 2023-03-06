d357cd3ad5f5ae11cfbbc30e5f0a2d31e874b233
Vieira14N <ianvieira0411@gmail.com>
2023-03-05 21:44:55 -0300

    Buscar ip do usuário ao carregar a página

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-// Map initializer
+// OnLoad (DOMContentLoaded) Script
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+document.addEventListener('DOMContentLoaded', () => {
+    async function onLoadFetch(url) {
+        const response = await fetch(url)
+        const json = await response.json()
+        return json
+    }
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    const res = onLoadFetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_gD1G4vs2yaNkYhx6yU0GkGpHWaPsh")
 
-var marker = L.marker([51.5, -0.09]).addTo(map)
+    res.then(r => {
+
+        const ip = document.querySelector('.ip')
+        const location = document.querySelector('.location')
+        const timezone = document.querySelector('.timezone')
+        const isp = document.querySelector('.isp')
+
+        ip.innerHTML = r.ip
+        location.innerHTML = `${r.location.city},${r.location.region}`
+        timezone.innerHTML = `UTC ${r.location.timezone}`
+        isp.innerHTML = r.isp
+
+        const lat = r.location.lat
+        const lng = r.location.lng
+
+        var map = L.map('map').setView([lat, lng], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        var marker = L.marker([lat, lng]).addTo(map)
+    })
+})
+
